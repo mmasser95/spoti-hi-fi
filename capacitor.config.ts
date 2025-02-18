2d15942ed72f97d0435465c1dc6b6d7499016b42
mmasser95 <mmasser95@gmail.com>
2025-02-18 17:12:38 +0100

    FIX: CapacitorSQLite plugin configuration by commenting out unused iOS and Electron settings

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -13,23 +13,23 @@ const config: CapacitorConfig = {
   },
   plugins:{
     CapacitorSQLite: {
-      iosDatabaseLocation: 'Library/CapacitorDatabase',
-      iosIsEncryption: true,
-      iosKeychainPrefix: 'angular-sqlite-app-starter',
-      iosBiometric: {
-        biometricAuth: false,
-        biometricTitle : "Biometric login for capacitor sqlite"
-      },
+      // iosDatabaseLocation: 'Library/CapacitorDatabase',
+      // iosIsEncryption: true,
+      // iosKeychainPrefix: 'angular-sqlite-app-starter',
+      // iosBiometric: {
+      //   biometricAuth: false,
+      //   biometricTitle : "Biometric login for capacitor sqlite"
+      // },
       androidIsEncryption: true,
       androidBiometric: {
         biometricAuth : false,
         biometricTitle : "Biometric login for capacitor sqlite",
         biometricSubTitle : "Log in using your biometric"
       },
-      electronIsEncryption: true,
-      electronWindowsLocation: "C:\\ProgramData\\CapacitorDatabases",
-      electronMacLocation: "/Volumes/Development_Lacie/Development/Databases",
-      electronLinuxLocation: "Databases"
+      // electronIsEncryption: true,
+      // electronWindowsLocation: "C:\\ProgramData\\CapacitorDatabases",
+      // electronMacLocation: "/Volumes/Development_Lacie/Development/Databases",
+      // electronLinuxLocation: "Databases"
     }
   }
 };
